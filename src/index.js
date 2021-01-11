1fb57f04ecbdefb1fbb4e30d2f3c40bd0f9cc02b
ImWonbin <dladnjs333@gmail.com>
2021-01-11 21:47:25 +0900

    Assets rename

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+/* Initialize Firebase
+var config = {
+  apiKey:"ABCDEFG",
+  authDomain:"fir-example-ec491.firebaseapp.com",
+  databaseURL:"https://fir-example-ec491.firebaseio.com",
+  projectId:"fir-example-ec491",
+  storageBucket:"fir-example-ec491.appspot.com",
+  messagingSenderId:""
+};
+firebase.initializeApp(config);
+
+var newPostKey = firebase.database().ref().child('posts').push().key;
+
+var postData = {
+  brdno: newPostKey,
+  Letterskin:"편지지 종류",
+  sticker:"스티커",
+  brdmemo:"게시물 내용",
+  like:"하트"
+  brddate: Date.now()
+};
+
+
+*/
+
+출처: https://forest71.tistory.com/165 [SW 개발이 좋은 사람]
+
 function letterPopup() {
 	const popup1 = document.querySelector('#letterPopup');
 
