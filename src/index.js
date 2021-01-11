443265dc5d7fdc110244cbc55d61019dbb925edb
ImWonbin <dladnjs333@gmail.com>
2021-01-11 21:07:43 +0900

    PopUp

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function letterPopup() {
+	const popup1 = document.querySelector('#letterPopup');
+
+  popup1.classList.remove('hide');
+}
+
+function closeLetterPopup() {
+	const popup2 = document.querySelector('#letterPopup');
+  popup2.classList.add('hide');
+}
+function writePopup() {
+	const popup3 = document.querySelector('#writePopup');
+
+  popup3.classList.remove('hide');
+}
+
+function closeWritePopup() {
+	const popup4 = document.querySelector('#writePopup');
+  popup4.classList.add('hide');
+}
