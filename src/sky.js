9981a6b15eb5ac3a6f3ada3b1c5b3fe250414838
jeonggyeong <zin511@naver.com>
2021-01-13 21:30:42 +0900

    sky 1
    Signed-off-by: jeonggyeong <zin511@naver.com>

---
+++ src/sky.js
@@ -0,0 +1,35 @@
+
+var current = new Date(); // 현재 시간 값을 가진 Date 객체 생성
+if(current.getHours() < 4) {
+    document.body.style.backgroundColor = "#000000"; //검정
+}
+else if(current.getHours() < 6) {
+    document.body.style.backgroundColor = "#000093"; //남색
+}
+ else if(current.getHours() < 8) {
+    document.body.style.backgroundColor = "#1266FF"; //파랑
+}
+else if(current.getHours() < 10) {
+    document.body.style.backgroundColor = "#5AAEFF"; //하늘
+}
+else if(current.getHours() < 14) {
+    document.body.style.backgroundColor = "#90E4FF"; //연한하늘
+}
+else if(current.getHours() < 16) {
+    document.body.style.backgroundColor = "#5AAEFF"; //하늘
+}
+else if(current.getHours() < 19) {
+    document.body.style.backgroundColor = "#1266FF"; //파랑
+}
+else if(current.getHours() < 20) {
+    document.body.style.backgroundColor = "#000093"; //남색
+}
+else  {
+    document.body.style.backgroundColor = "#000000"; //검정
+}
+
+document.write("현재 시간 : ");
+document.write(current.getHours(), "시,");
+document.write(current.getMinutes(), "분,");
+document.write(current.getSeconds(), "초<br>");
+
---
+++ src/weather1.js
@@ -0,0 +1,76 @@
+
+/*
+function show {}
+document.getElementsByClassName("snowflakes").style.display="block";
+document.getElementsByClassName("clouds").style.display="none";
+document.getElementsByClassName("rain").style.display="none";
+document.getElementsByClassName("sun").style.display="none";
+document.getElementsByClassName("moon").style.display="none";
+*/
+
+/*
+$(".snowflakes").show();
+$(".clouds").hide();
+$(".rain").hide();
+$(".sun").hide();
+$(".moon").hide();
+*/
+   
+
+/*
+if(id < 600) {
+    document.getElementsByClassName("snowflakes").style.display= "none";
+    document.getElementsByClassName("clouds").style.display= "none";
+    document.getElementsByClassName("rain").style.display= "block";
+    document.getElementsByClassName("sun").style.display= "none";
+    document.getElementsByClassName("moon").style.display= "none";
+}
+else if(id < 700) {
+    document.getElementsByClassName("snowflakes").style.display= "block";
+    document.getElementsByClassName("clouds").style.display= "none";
+    document.getElementsByClassName("rain").style.display= "none";
+    document.getElementsByClassName("sun").style.display= "none";
+    document.getElementsByClassName("moon").style.display= "none";
+}
+else if(id < 800) {
+    document.getElementsByClassName("snowflakes").style.display= "none";
+    document.getElementsByClassName("clouds").style.display= "block";
+    document.getElementsByClassName("rain").style.display= "none";
+    document.getElementsByClassName("sun").style.display= "none";
+    document.getElementsByClassName("moon").style.display= "none";
+}
+else {
+    document.getElementsByClassName("snowflakes").style.display= "none";
+    document.getElementsByClassName("clouds").style.display= "none";
+    document.getElementsByClassName("rain").style.display= "none";
+    document.getElementsByClassName("sun").style.display= "block";
+    document.getElementsByClassName("moon").style.display= "block";
+}
+*/
+
+
+   
+    function doDisplay() {
+    document.getElementsByClassName("snowflakes").style.display= "block";
+    document.getElementsByClassName("clouds").style.display= "none";
+    document.getElementsByClassName("rain").style.display= "none";
+    document.getElementsByClassName("sun").style.display= "none";
+    document.getElementsByClassName("moon").style.display= "none";
+
+
+    document.getElementsById("snowflakes").style.display= "none";
+        document.getElementsByClassName("snowflake").style.display= "none";
+        document.getElementsByClassName("clouds").style.display= "none";
+        document.getElementsByClassName("cloud").style.display= "none";
+        document.getElementsByClassName("cloud x1").style.display= "none";
+        document.getElementsByClassName("cloud x2").style.display= "none";
+        document.getElementsByClassName("cloud x3").style.display= "none";
+        document.getElementsByClassName("cloud x4").style.display= "none";
+        document.getElementsByClassName("cloud x5").style.display= "none";
+        document.getElementsByClassName("rain").style.display= "none";
+        document.getElementsByClassName("sun").style.display= "none";
+        document.getElementsByClassName("moon").style.display= "none";
+    }
+
+
+    
