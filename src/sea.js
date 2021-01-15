d615ebf6fce4f28ba49b35ea506c38fb07e82513
ManaBingsu <43133819+ManaBingsu@users.noreply.github.com>
2021-01-15 21:02:29 +0900

    feat : Spawn bottle, clean field

--- src/sea.js
+++ src/sea.js
@@ -1,30 +1,82 @@
-var inputBox = document.getElementById("inputBox");
-var saveButton = document.getElementById("saveButton");
-saveButton.addEventListener('click', saveLetter);
-var loadButton = document.getElementById("loadButton");
-loadButton.addEventListener('click', loadLetter);
-var letter = document.getElementById("letter");
+var bottleNum = 0;
+var maxBottleNum = 8;
+var spawnInterval = 500;
+var field;
+var fieldWidth;
+var fieldHeight;
+var wave;
 
-function saveLetter() {
+initialize();
 
-    var newPostKey = firebase.database().ref().child('letters').push().key;
-    firebase.database().ref('letters/' + newPostKey).set({
-        content: inputBox.value
+function initialize() {
+    field = document.getElementById('beach');
+    fieldWidth = field.offsetWidth;
+    fieldHeight = field.offsetHeight;
+
+    wave = document.getElementById('wave');
+    wave.addEventListener('animationend', () => {
+        wave.classList.remove('wave');
+        void wave.offsetWidth;
+        wave.classList.add('wave');
+        wave.style.animationPlayState = "paused";
     });
 
-    console.log(inputBox.value + " was succesfully saved");
+    const animated = document.querySelector('.wave');
+    animated.addEventListener('animationend', () => {
+        var target = wave[0];
+        target.classList.remove('wave');
+        void target.offsetWidth;
+        target.classList.add('wave');
+        target.style.animationPlayState = "paused";
+    });
+
+    spawnBottles();
 }
 
-function loadLetter() {
-    firebase.database().ref().child('letters').once('value').then((snapshot) => {
-        var length = snapshot.numChildren();
-        var randNum = Math.floor( Math.random() * 10 ) % length;
-        var count = 0;
-        snapshot.forEach(function(child) {
-            if (count == randNum) {
-                letter.innerHTML = child.val().content;
-            }
-            count += 1;
-        });
-    });
+function cleanField() {
+    var style = wave.style;
+    style.animationPlayState = "running";
+    setTimeout(cleanBottle, 500);
+    setTimeout(spawnBottles, 500);
+}
+
+function spawnBottle() {
+    if (bottleNum >= maxBottleNum)
+        return;
+
+    var bottle = document.createElement('button')
+    
+    bottle.className = "bottle";
+    bottle.style.position = 'absolute';
+    bottle.style.left = String((Math.random()*100)%100) + '%';
+    bottle.style.top = String((Math.random()*100)%100) + '%';
+    bottle.style.zIndex = String(count);
+
+    var image = document.createElement('img');
+    image.className = "bottleImage";
+    // image.src = './image/letter.svg';
+    bottle.appendChild(image);
+    field.appendChild(bottle);
+
+    bottleNum += 1;
+}
+
+function spawnBottles() {
+    for (var i = bottle; i < maxBottleNum; i++) {
+        spawnBottle();
+    }
+}
+
+function deleteBottle(bottle) {
+    bottle.parentNode.removeChild(bottle);
+}
+
+function cleanBottle() {
+    var child = field.lastElementChild;  
+        while (child) { 
+            deleteBottle(child);
+            // beach.removeChild(child); 
+            child = field.lastElementChild; 
+            bottleNum -= 1;
+        } 
 }
